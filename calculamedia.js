34ad919c683e2806b43a36174f04c0ed0e875f95
wettooo777 <welliton.souza.oliveira@escola.pr.gov.br>
2026-02-09 13:04:46 +0000

    só isso para o front-end

---
+++ calculamedia.js
@@ -0,0 +1,18 @@
+let nota1 = 10;
+let nota2 = 9;
+let nota3 = 8;
+let nota4 = 7;
+
+const nomealuno = "mei kilu";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log ("O aluno: " + nomealuno);
+console.log ("Tem a média: " + media);
+
+
+
+
+
+
+
